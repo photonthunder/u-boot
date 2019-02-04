16a26aad3896916a49e5b08f670d2422ac0f7980
Daniel Evans <photonthunder@gmail.com>
2019-02-04 14:47:53 -0700

    Fix env_offset

--- include/configs/sama5d3_emtrion.h
+++ include/configs/sama5d3_emtrion.h
@@ -54,8 +54,7 @@
 
 #ifdef CONFIG_NAND_BOOT
 /* u-boot env in nand flash */
-#define CONFIG_ENV_OFFSET        0x140000
-#define CONFIG_ENV_OFFSET_REDUND    0x100000
+#define CONFIG_ENV_OFFSET        0x100000
 #define CONFIG_ENV_SIZE            0x20000
 #define CONFIG_BOOTCOMMAND        "nand read 0x21000000 0x180000 0x80000;"    \
 "nand read 0x22000000 0x200000 0x600000;"    \
